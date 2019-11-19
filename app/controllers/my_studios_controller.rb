05db5c77d6a7c1561d9c53101258f5098f2e7625
Lynn Qi qilin2481@gmail.com <qilin2481@gmail.com>
2019-11-19 14:37:11 -0500

    corrected controller  my_studios: new, create. added: edit, update, destroy and show.

--- app/controllers/my_studios_controller.rb
+++ app/controllers/my_studios_controller.rb
@@ -6,12 +6,14 @@ class MyStudiosController < ApplicationController
 
   def new
     user = current_user
-    @my_studio = Studio.new(user)
+    @my_studio = Studio.new
+    @my_studio.user = user
   end
 
   def create
     user = current_user
-    @my_studio = Studio.new(user)
+    @my_studio = Studio.new(params_my_studio)
+    @my_studio.user = user
     if @my_studio.save
       redirect_to my_studios_path
     else
@@ -20,14 +22,31 @@ class MyStudiosController < ApplicationController
   end
 
   def edit
+    user = current_user
+    @my_studio = Studio.find(user.studios.id = params[:id])
   end
 
   def update
+    user = current_user
+    @my_studio = Studio.find(user.studios.id = params[:id])
+    @my_studio.update(params_my_studio)
   end
 
   def destroy
+    user = current_user
+    @my_studio = Studio.find(user.studios.id = params[:id])
+    @my_studio.destroy
+    redirect_to my_studios_path
   end
 
   def show
+    user = current_user
+    @my_studio = Studio.find(user.studios.id = params[:id])
+  end
+
+  private
+
+  def params_my_studio
+    params.require(:studio).permit(:name, :category, :price_per_hour, :city, :description)
   end
 end
