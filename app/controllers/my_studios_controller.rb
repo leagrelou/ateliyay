0add1a168c6df03180470aa27262b66f472b98f8
Lynn Qi qilin2481@gmail.com <qilin2481@gmail.com>
2019-11-19 14:05:08 -0500

    add my_studios action index, new, create

--- app/controllers/my_studios_controller.rb
+++ app/controllers/my_studios_controller.rb
@@ -1,11 +1,22 @@
 class MyStudiosController < ApplicationController
   def index
+    user = current_user
+    @my_studios = user.studios
   end
 
   def new
+    user = current_user
+    @my_studio = Studio.new(user)
   end
 
   def create
+    user = current_user
+    @my_studio = Studio.new(user)
+    if @my_studio.save
+      redirect_to my_studios_path
+    else
+      render :new
+    end
   end
 
   def edit
