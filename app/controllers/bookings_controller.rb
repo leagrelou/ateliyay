60f2244e1f764a873dd1a7161346da557d89c559
Lea Grelou <leagrelou@gmail.com>
2019-11-19 15:06:30 -0500

    doing the booking controller + views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,29 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @studio = set_studio
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @studio = set_studio
+    @booking.studio = @studio
+    @booking.user = current_user
+    @booking.save
+
+    redirect_to booking_path(@booking)
   end
 
   def show
+  end
+
+  private
+
+  def set_studio
+    Studio.find(params[:studio_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_datetime, :end_datetime)
   end
 end
